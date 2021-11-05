4f2d282cb35758bad469e5a3cd2a48d297cbd150
isko820 <istemi820@gmail.com>
2021-11-05 21:14:05 +0600

    Task

---
+++ task.psql.py
@@ -0,0 +1,108 @@
+# create table competition (
+# 	id SERIAL PRIMARY KEY,
+# 	competition_name VARCHAR(50),
+# 	world_record INTEGER(50),
+# 	set_date INTEGER(50)
+# );
+# insert into competition (id, competition_name, world_record, set_date) values (1, 'marathon', 4, 20.05.2005);
+# insert into competition (id, competition_name, world_record, set_date) values (2, 'race walking', 5, 31.08.2019);
+# insert into competition (id, competition_name, world_record, set_date) values (3, 'swimming', 7, 23.06.2014);
+# insert into competition (id, competition_name, world_record, set_date) values (4, 'karate', 3, 25.09.2013);
+# insert into competition (id, competition_name, world_record, set_date) values (5, 'judo', 8, 13.05.2013);
+#
+#
+# create table sportsman (
+# 	sportsman_id SERIAL PRIMARY KEY,
+# 	sportsman_name VARCHAR(50),
+# 	rank INT,
+# 	year_of_birth  VARCHAR(50),
+# 	personal_record INT,
+# 	country VARCHAR(50)
+# );
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (1, 'Bancroft', 1, 1988, 1, 'China');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (2, 'Nappie', 2, 2002, 2, 'Egypt');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (3, 'Agna', 3, 1960, 3, 'China');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (4, 'Annabell', 4, 2011, 4, 'Guatemala');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (5, 'Donovan', 5, 2012, 5, 'Indonesia');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (6, 'Dani', 6, 2005, 6, 'Canada');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (7, 'Clive', 7, 1995, 7, 'Ecuador');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (8, 'Reinold', 8, 2011, 8, 'Lesotho');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (9, 'Shannon', 9, 2004, 9, 'China');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (10, 'Davidson', 10, 2010, 10, 'China');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (11, 'Ancell', 11, 2010, 11, 'Nicaragua');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (12, 'Phylis', 12, 2008, 12, 'Indonesia');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (13, 'Nicol', 13, 2005, 13, 'Serbia');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (14, 'Obed', 14, 1998, 14, 'Colombia');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (15, 'Anabelle', 15, 1989, 15, 'South Africa');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (16, 'Niles', 16, 1993, 16, 'Russia');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (17, 'Bab', 17, 2001, 17, 'Philippines');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (18, 'Rollo', 18, 2001, 18, 'Poland');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (19, 'Nancee', 19, 1995, 19, 'France');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (20, 'Emelen', 20, 2010, 20, 'United States');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (21, 'Audrye', 21, 1998, 21, 'Philippines');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (22, 'Ignacius', 22, 2004, 22, 'Nigeria');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (23, 'Katharina', 23, 2008, 23, 'Poland');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (24, 'Cordula', 24, 1993, 24, 'China');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (25, 'Cordelia', 25, 1991, 25, 'Luxembourg');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (26, 'Gabriello', 26, 2011, 26, 'China');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (27, 'Hedvige', 27, 1997, 27, 'Portugal');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (28, 'Kerianne', 28, 1998, 28, 'Russia');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (29, 'Stacy', 29, 1961, 29, 'Laos');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (30, 'Gilda', 30, 2013, 30, 'Peru');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (31, 'Emmalee', 31, 1998, 31, 'Russia');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (32, 'Shanie', 32, 2009, 32, 'United States');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (33, 'Marga', 33, 1992, 33, 'Mexico');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (34, 'Brigit', 34, 1999, 34, 'China');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (35, 'Sarene', 35, 1993, 35, 'Japan');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (36, 'Porty', 36, 2005, 36, 'Germany');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (37, 'Jarred', 37, 2003, 37, 'Russia');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (38, 'Juliann', 38, 1996, 38, 'China');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (39, 'Philippine', 39, 2012, 39, 'China');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (40, 'Page', 40, 1998, 40, 'Nicaragua');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (41, 'Rebbecca', 41, 2008, 41, 'Portugal');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (42, 'Cariotta', 42, 2005, 42, 'Russia');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (43, 'Dwain', 43, 2002, 43, 'Morocco');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (44, 'Lizabeth', 44, 2010, 44, 'Albania');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (45, 'La verne', 45, 2003, 45, 'United States');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (46, 'Silvie', 46, 1996, 46, 'Nigeria');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (47, 'Elvin', 47, 1989, 47, 'China');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (48, 'Boone', 48, 2011, 48, 'Guatemala');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (49, 'Winnah', 49, 2006, 49, 'Russia');
+# insert into sportsman (sportsman_id, sportsman_name, rank, year_of_birth , personal_record, country) values (50, 'Jeffry', 50, 1986, 50, 'Philippines');
+#
+# create table result (
+# 	competition_id  SERIAL PRIMARY KEY,
+# 	sportsman_id INTEGER REFERENCES sportsman (Id)
+# 	result INT,
+# 	city VARCHAR(50),
+# 	hold_date DATE
+# );
+# insert into result (competition_id , sportsman_id, result, city, hold_date) values (1, 1, 1, 'Russia', '3/20/2021');
+# insert into result (competition_id , sportsman_id, result, city, hold_date) values (2, 2, 2, 'Russia', '3/12/2021');
+# insert into result (competition_id , sportsman_id, result, city, hold_date) values (3, 3, 3, 'Russia', '11/11/2020');
+# insert into result (competition_id , sportsman_id, result, city, hold_date) values (4, 4, 4, 'Russia', '5/27/2021');
+# insert into result (competition_id , sportsman_id, result, city, hold_date) values (5, 5, 5, 'Russia', '7/22/2021');
+#
+# create table competition (
+# 	id SERIAL PRIMARY KEY,
+# 	competition_name VARCHAR(50),
+# 	world_record INTEGER(50),
+# 	set_date DATE
+# );
+# insert into competition (id, competition_name, world_record, set_date) values (1, 'wrestling', 4, 20.05.2005);
+# insert into competition (id, competition_name, world_record, set_date) values (2, 'Boxes', 5, 31.08.2019);
+# insert into competition (id, competition_name, world_record, set_date) values (3, 'fight', 7, 23.06.2014);
+# insert into competition (id, competition_name, world_record, set_date) values (4, 'karate', 3, 25.09.2013);
+# insert into competition (id, competition_name, world_record, set_date) values (5, 'judo', 8, 13.05.2013);
+#
+# SELECT * FROM sportsman;
+#
+# SELECT competition_id, world_record FROM  competition;
+#
+# SELECT * FROM sportsman WHERE year_of_birth = 2000;
+#
+# SELECT * FROM competition WHERE competition_name and world_record = 12-05-2010 or 15-05-2010;
+#
+# SELECT * FROM sportsman WHERE NOT personal_record = 25;
+#
+# SELECT COUNT(*) FROM sportsman WHERE sportsman_name = "Ivanov"
